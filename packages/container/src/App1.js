6728dccfe7eb632e6429411d2a99dd3462bc69ce
jeffkk1 <jkaufman512@gmail.com>
2025-08-10 15:48:47 -0400

    quotes removed region name

--- packages/container/src/App1.js
+++ packages/container/src/App1.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import mountMe from 'marketing/Marketing'
 import MarketingApp from "./components/MarketingApp";
-
 export default () => {
   // console.log('mount:', mountMe)
     return (
