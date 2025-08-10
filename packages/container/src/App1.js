f604cd9f128e9a0de11089e0e1da3254e1d8aacc
jeffkk1 <jkaufman512@gmail.com>
2025-08-10 10:31:05 -0400

    added permissions for list bucket

--- packages/container/src/App1.js
+++ packages/container/src/App1.js
@@ -2,9 +2,6 @@ import React from 'react'
 import mountMe from 'marketing/Marketing'
 import MarketingApp from "./components/MarketingApp";
 
-//console.log('mount:', mountMe)
-//console.log('App/Mount:', mount)
-
 export default () => {
   // console.log('mount:', mountMe)
     return (
