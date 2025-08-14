8b5879d0993f8bfe0ae150df5a313633c87fbce0
jeffkk1 <jkaufman512@gmail.com>
2025-08-14 13:53:34 -0400

    added css to js file fix

--- packages/container/src/App1.js
+++ packages/container/src/App1.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { BrowserRouter } from "react-router-dom"
+import { StylesProvider, createGenerateClassName } from '@material-ui/core'
 import mountMe from 'marketing/Marketing'
 import MarketingApp from "./components/MarketingApp";
 import Header from './components/Header'
+
+const generateClassName = createGenerateClassName({ 
+    productionPrefix: 'co',
+})
+
 export default () => {
   // console.log('mount:', mountMe)
     return (
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <div>
           <Header/>
           <MarketingApp/> 
         </div>
       </BrowserRouter>
+    </StylesProvider>
     )
 }
