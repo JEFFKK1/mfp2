363c64d4d51e26078c5bfb215f4b0630fcfde09b
jeffkk1 <jkaufman512@gmail.com>
2025-08-14 16:42:43 -0400

    amother try for ma

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,6 +1,5 @@
 import React, {useRef, useEffect} from 'react'
 import mountMe from 'marketing/Marketing'
-//console.log('marketingApp/mount: ', mountMe)
 export default () => {
     const ref = useRef(null)
  //   console.log('what is ref:', ref)
