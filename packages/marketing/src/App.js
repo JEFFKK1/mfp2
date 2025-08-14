3690d3529a88bfd473dd14499f13e6884e7894cd
jeffkk1 <jkaufman512@gmail.com>
2025-08-14 16:08:17 -0400

    modify ma

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -5,21 +5,21 @@ import Landing from './components/Landing'
 import Pricing from  './components/Pricing'
 
 const generateClassName = createGenerateClassName({ 
-    productionPrefix: 'ma',
+    productionPrefix: 'ma'
 })
 
 export default () => {
     console.log('marketing app.js')
     return (
+    <StylesProvider generateClassName={generateClassName}>
         <div>
-            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path= '/Pricing' component={Pricing}/>
                         <Route path ='/' component={Landing}/>
                     </Switch>
                 </BrowserRouter>
-            </StylesProvider>
         </div>
+    </StylesProvider>
     )
 }
